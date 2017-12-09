71afa8c51f8837f69e6532472a0b05d1d0947c29
Laurence <ljwoodward.code@gmail.com>
2017-12-09 22:43:05 +0000

    created test for rat class

---
+++ specs/rat_specs.js
@@ -0,0 +1,22 @@
+var assert = require('assert');
+var Rat = require('../rat.js');
+var Food = require('../food.js');
+
+describe('Rat', function() {
+
+  let rat;
+  let food1;
+
+  beforeEach(function () {
+    rat = new Rat();
+    food1 = new Food("Mashed Potatoes", 5);
+  })
+  // - Create a constructor to create Rat objects.
+  // - Rats should be able to touch food,  if they do the food becomes poisonous.
+  it("should be able to touch food", function () {
+    rat.touchFood(food1);
+    assert.strictEqual(-50, food1.replenValue);
+  })
+
+
+})
